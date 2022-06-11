f59e2c6e23442646cc40a9479fa5608c6544f113
Marek Krysiak <106902757+MarcusMKappa@users.noreply.github.com>
2022-06-11 11:49:57 +0200

    Add files via upload

---
+++ Python programming A to Z/03_select_from.sql
@@ -0,0 +1,21 @@
+SELECT * FROM Category;
+
+SELECT  Id, CategoryName, Description FROM Category;
+
+SELECT  Id,
+		CategoryName, 
+		Description 
+FROM Category;
+
+SELECT  CategoryName, 
+		Description 
+FROM Category;
+
+SELECT  Id,
+		CategoryName as Category, 
+		Description 
+FROM Category;
+
+SELECT * from Employee;
+
+SELECT FirstName, LastName, City, Title from Employee;
