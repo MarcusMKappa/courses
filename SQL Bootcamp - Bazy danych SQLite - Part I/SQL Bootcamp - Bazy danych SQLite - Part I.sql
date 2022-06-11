5bf89f086b903aa1e9f8234ecfb106372674b6dd
Marek Krysiak <106902757+MarcusMKappa@users.noreply.github.com>
2022-06-11 12:14:31 +0200

    Add files via upload

---
+++ SQL Bootcamp - Bazy danych SQLite - Part I/SQL Bootcamp - Bazy danych SQLite - Part I.sql
@@ -0,0 +1,88 @@
+/* SQL Bootcamp - Bazy danych SQLite - Part I */
+
+/* Instrukcja SELECT */
+
+SELECT 20;
+
+SELECT 20 + 40;
+
+SELECT 20 - 40;
+
+SELECT 20 * 40;
+
+SELECT 20 / 5;
+
+SELECT 21 / 5.0; 
+
+SELECT 20 % 5;
+
+SELECT 21 % 5;  
+
+SELECT 'python'; 
+
+SELECT 'python', 'java', 'c++'; 
+
+SELECT 'Sci' || ' & ', 'Tech';
+
+SELECT True, False;
+
+SELECT NULL;
+
+SELECT (45.0 / 42.0) - 1;
+
+SELECT (45.0 / 42.0) - 1 AS daily_return;
+
+SELECT (45.0 / 42.0) - 1 AS daily_return,
+		'GPW' AS stock_market
+
+
+/* Instrukcja SELECT ... FROM ... */
+
+
+
+/* Instrukcja SELECT ... FROM ... */
+
+
+/* Instrukcja ORDER BY */
+
+
+/* Instrukcja LIMIT + OFFSET */
+
+
+/* Instrukcja SELECT DISTINCT */
+
+
+/* Instrukcja WHERE */
+
+
+/* Instrukcja SELECT CASE */
+
+
+/* Podzapytania */
+
+
+/* Łączenie tabel */
+
+
+/* Połączenie typy CROSS JOIN */
+
+
+/* Połączenie typy LEFT JOIN */
+
+
+/* Połączenie typy INNER JOIN */
+
+
+/* Połączenie typy SELF JOIN */
+
+
+/* Łączenie tabel UNION */
+
+
+/* Łączenie tabel UNION ALL */
+
+
+/* Łączenie tabel EXCEPT */
+
+
+/* Łączenie tabel INTERSECT */
