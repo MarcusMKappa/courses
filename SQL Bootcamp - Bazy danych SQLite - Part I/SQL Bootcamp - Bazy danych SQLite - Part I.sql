4a7edd95b1079b172279dc1fca7d26169e14cd1b
Marek Krysiak <106902757+MarcusMKappa@users.noreply.github.com>
2022-06-11 13:48:09 +0200

    Add files via upload

--- SQL Bootcamp - Bazy danych SQLite - Part I/SQL Bootcamp - Bazy danych SQLite - Part I.sql
+++ SQL Bootcamp - Bazy danych SQLite - Part I/SQL Bootcamp - Bazy danych SQLite - Part I.sql
@@ -80,11 +80,120 @@ FROM
 	Employee;
 
 
-/* Instrukcja SELECT ... FROM ... */
 
 
 /* Instrukcja ORDER BY */
 
+SELECT 
+	* 
+FROM 
+	Employee
+ORDER BY 
+	FirstName;
+
+	
+SELECT 
+	* 
+FROM 
+	Employee
+ORDER BY 
+	FirstName DESC;
+
+
+SELECT 
+	* 
+FROM 
+	Employee
+ORDER BY 
+	Title, 
+	FirstName;
+
+	
+SELECT 
+	* 
+FROM 
+	Employee
+ORDER BY 
+	Title, 
+	FirstName DESC;
+
+
+SELECT 
+	Title,
+	FirstName,
+	LastName
+FROM 
+	Employee
+ORDER BY 
+	1, 
+	2 DESC;
+
+
+SELECT 
+	* 
+FROM 
+	Product
+ORDER BY 
+	UnitPrice;
+	
+
+SELECT 
+	* 
+FROM 
+	Product
+ORDER BY 
+	UnitPrice DESC;
+	
+	
+SELECT 
+	* 
+FROM 
+	Customer
+ORDER BY 
+	Fax;
+
+	
+SELECT 
+	CompanyName,
+	ContactName,
+	Fax
+FROM 
+	Customer
+ORDER BY 
+	Fax;
+	
+	
+SELECT 
+	CompanyName,
+	ContactName,
+	Fax
+FROM 
+	Customer
+ORDER BY 
+	Fax DESC;
+	
+		
+SELECT 
+	CompanyName,
+	ContactName,
+	Fax
+FROM 
+	Customer
+ORDER BY 
+	Fax DESC NULLS FIRST;
+
+
+SELECT 
+	CompanyName,
+	ContactName,
+	Fax
+FROM 
+	Customer
+ORDER BY 
+	Fax ASC NULLS LAST;
+	
+	
+	
 
 /* Instrukcja LIMIT + OFFSET */
 
